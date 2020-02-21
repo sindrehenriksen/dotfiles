b478a1f5a3caf10cc5c913ec338ae359a34441ed
sindrehenriksen <sindrehenriksen93@gmail.com>
2020-02-21 09:05:50 +0100

    Add .zprofile

--- .zshrc
+++ .zshrc
@@ -1,4 +1,5 @@
 ## Init
+. ~/.zprofile
 source ~/.zplug/init.zsh
 
 ## General
--- install_symlinks.sh
+++ install_symlinks.sh
@@ -5,4 +5,5 @@ ln -is ~/dotfiles/.shellrc ~/.shellrc
 ln -is ~/dotfiles/.ssh/config ~/.ssh/config
 ln -is ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -is ~/dotfiles/.vimrc ~/.vimrc
+ln -is ~/dotfiles/.zprofile ~/.zprofile
 ln -is ~/dotfiles/.zshrc ~/.zshrc
