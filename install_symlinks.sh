5e4121cefd24a0571f1cff1a05068ab9f55ca9db
sindrehenriksen <sindrehenriksen93@gmail.com>
2021-03-04 08:55:07 +0100

    Update symlink install script

--- install_symlinks.sh
+++ install_symlinks.sh
@@ -2,8 +2,9 @@ ln -is ~/dotfiles/.bashrc ~/.bashrc
 ln -is ~/dotfiles/.gitconfig ~/.gitconfig
 ln -is ~/dotfiles/.profile ~/.profile
 ln -is ~/dotfiles/.shellrc ~/.shellrc
-ln -is ~/dotfiles/.ssh/config ~/.ssh/config
 ln -is ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -is ~/dotfiles/.vimrc ~/.vimrc
 ln -is ~/dotfiles/.zprofile ~/.zprofile
 ln -is ~/dotfiles/.zshrc ~/.zshrc
+mkdir -p ~/.jupyter
+ln -is ~/dotfiles/.jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
